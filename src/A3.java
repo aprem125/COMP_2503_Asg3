c33cc55953f83711b10c2ed8146115eb2fbe5b58
aleempremji <aleempremji@192.168.68.89>
2023-03-20 14:29:51 -0600

    update

--- src/A3.java
+++ src/A3.java
@@ -1,13 +1,16 @@
+
 import java.util.Scanner;
 
 /**
- * COMP 2503 Winter 2020 Assignment 2
+ * COMP 2503 Winter 2023 Assignment 3
  * 
  * This program must read a input stream and keeps track of the frequency at
- * which an avenger is mentioned either by name or alias.
- *
+ * which an avenger is mentioned either by name or alias. The program uses a BST
+ * for storing the data. Multiple BSTs with alternative orderings are
+ * constructed to show the required output.
+ * 
  * @author Maryam Elahi
- * @date Fall 2020
+ * @date Winter 2023
  */
 
 public class A3 {
@@ -20,160 +23,90 @@ public class A3 {
 	private int topN = 4;
 	private int totalwordcount = 0;
 	private Scanner input = new Scanner(System.in);
+//	private BST<Avenger> alphabticalBST = new BST<>();
+//	private BST<Avenger> mentionBST = new BST<Avenger>(new AvengerComparatorMentionOrder());
+//	private BST<Avenger> mostPopularBST = new BST<Avenger>(new AvengerComparatorFreqDesc());
+//	private BST<Avenger> leastPopularBST = new BST<Avenger>(new AvengerComparatorFreqAsc());
 
-	private SLL<Avenger> mentionList = new SLL<Avenger>();
-	private SLL<Avenger> alphabticalList = new SLL<Avenger>(new AvengerAlphaComparator());
-	private SLL<Avenger> mostPopularList = new SLL<Avenger>(new AvengerComparatorFreqDesc());
-	private SLL<Avenger> leastPopularList = new SLL<Avenger>(new AvengerComparatorFreqAsc());
-
-	/**
-	 * main method
-	 * 
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		A3 a3 = new A3();
-		a3.run();
+		A3 a1 = new A3();
+		a1.run();
 	}
 
 	public void run() {
-
 		readInput();
-		createdOrderedLists();
+		createdAlternativeOrderBSTs();
 		printResults();
 	}
 
-	/**
-	 * this method takes the avengers in the mention list and transfer them in into
-	 * a ordered lists
-	 * 
-	 */
-	private void createdOrderedLists() {
-		Node<Avenger> current = mentionList.getHead();
-		while (current != null) {
-			mostPopularList.addInOrder(current.getData());
-			alphabticalList.addInOrder(current.getData());
-			leastPopularList.addInOrder(current.getData());
-			current = current.getNext();
-		}
-
+	private void createdAlternativeOrderBSTs() {
+		/* TODO:
+		 *   - delete the avenger "hawkeye"/"barton" from the alphabetical tree
+		 *   use the tree iterator to do an in-order traversal of the alphabetical tree,
+		 *   and add avengers to the other trees
+		 */
 	}
 
 	/**
 	 * read the input stream and keep track how many times avengers are mentioned by
 	 * alias or last name.
-	 * 
-	 * @throws FileNotFoundException
 	 */
-	private void readInput() { // remove exception after testing
+	private void readInput() {
 		/*
-		 * In a loop, while the scanner object has not reached end of stream, - read a
-		 * word. - clean up the word - if the word is not empty, add the word count. -
-		 * Check if the word is either an avenger alias or last name then - Create a new
-		 * avenger object with the corresponding alias and last name. - if this avenger
-		 * has already been mentioned, increase the frequency count for the object
-		 * already in the list. - if this avenger has not been mentioned before, add the
-		 * newly created avenger to the end of the list, remember to set the frequency.
+		 * While the scanner object has not reached end of stream, 
+		 * 	- read a word. 
+		 * 	- clean up the word 
+		 * 	- if the word is not empty, add the word count. 
+		 * 	- Check if the word is either an avenger alias or last name then 
+		 *  - Create a new avenger object with the corresponding alias and last name. 
+		 *  - if this avenger has already been mentioned, increase the frequency count 
+		 *  for the object already in the list. 
+		 *  - if this avenger has not been mentioned before, add the 
+		 *  newly created avenger to the list, remember to set the frequency and the mention order.
+		 * (Remember to keep track of the mention order by setting the mention order for each new avenger.)
 		 */
-
-		while (input.hasNext()) {
-
-			String word = cleanWord(input.next());
-
-			if (!word.isEmpty()) {
-				totalwordcount++;
-
-				if (isAvenger(word)) {
-					Avenger avenger = createAvengerObject(word);
-					if (mentionList.contains(avenger)) {
-						mentionList.get(mentionList.indexOf(avenger)).addFrequency();
-					} else {
-						mentionList.addLast(avenger);
-					}
-				}
-			}
-		}
-	}
-
-	private Avenger createAvengerObject(String word) {
-		int inx = -1;
-		for (int i = 0; i < avengerRoster.length; i++) {
-			if (avengerRoster[i][0].equals(word) || avengerRoster[i][1].equals(word)) {
-				inx = i;
-				break;
-			}
-		}
-		if (inx != -1) {
-			return new Avenger(avengerRoster[inx][0], avengerRoster[inx][1], inx);
-		} else
-			return null;
-	}
-
-	private boolean isAvenger(String word) {
-		for (int i = 0; i < avengerRoster.length; i++) {
-			if ((avengerRoster[i][0].equals(word)) || (avengerRoster[i][1].equals(word))) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	/**
-	 * 
-	 * @param next
-	 * @return
-	 */
-	private String cleanWord(String next) {
-		// First, if there is an apostrophe, the substring
-		// before the apostrophe is used and the rest is ignored.
-		// Words are converted to all lowercase.
-		// All other punctuation and numbers are skipped.
-
-		String ret;
-		int inx = next.indexOf('\'');
-		if (inx != -1)
-			ret = next.substring(0, inx).toLowerCase().trim().replaceAll("[^a-z]", "");
-		else
-			ret = next.toLowerCase().trim().replaceAll("[^a-z]", "");
-		return ret;
-
 	}
 
 	/**
 	 * print the results
-	 *
 	 */
-	private void printResults() { // need to add number of avangers mentioned
-		System.out.println();
-		// SLL sll = new SLL();
-		// int S =sll.size();
+	private void printResults() {
 		System.out.println("Total number of words: " + totalwordcount);
-		System.out.println("Number of Avengers Mentioned: " + mentionList.size());
+		// TODO: Print the number of mentioned avengers after deleting "Barton".
+		//System.out.println("Number of Avengers Mentioned: " + ??);
 		System.out.println();
 
 		System.out.println("All avengers in the order they appeared in the input stream:");
-		// Todo: Print the list of avengers in the order they appeared in the input
+		// TODO: Print the list of avengers in the order they appeared in the input
 		// Make sure you follow the formatting example in the sample output
-
+		//
 		System.out.println();
-		mentionList.printList();
 
 		System.out.println("Top " + topN + " most popular avengers:");
-		// Todo: Print the most popular avengers, see the instructions for tie breaking
+		// TODO: Print the most popular avengers, see the instructions for tie breaking
 		// Make sure you follow the formatting example in the sample output
-		mostPopularList.printFour();
+		//
+		System.out.println();
 
 		System.out.println("Top " + topN + " least popular avengers:");
-		// Todo: Print the least popular avengers, see the instructions for tie breaking
+		// TODO: Print the least popular avengers, see the instructions for tie breaking
 		// Make sure you follow the formatting example in the sample output
-
+		//
 		System.out.println();
-		leastPopularList.printFour();
 
 		System.out.println("All mentioned avengers in alphabetical order:");
-		// Todo: Print the list of avengers in alphabetical order
-
+		// TODO: Print the list of avengers in alphabetical order
+		//
 		System.out.println();
-		alphabticalList.printList();
+
+		// TODO: Print the actual height and the optimal height for each of the four trees.
+//		System.out.println("Height of the mention order tree is : " + ??
+//				+ " (Optimal height for this tree is : " + ?? + ")");
+//		System.out.println("Height of the alphabetical tree is : " + ??
+//				+ " (Optimal height for this tree is : " + ?? + ")");
+//		System.out.println("Height of the most frequent tree is : " + ??
+//				+ " (Optimal height for this tree is : " + ?? + ")");
+//		System.out.println("Height of the least frequent tree is : " + ??
+//				+ " (Optimal height for this tree is : " + ?? + ")");
 	}
 }
